fca71ef9a1532ce1195696ae377e115a98d2b76b
zven21 <andzven@gmail.com>
2018-08-11 12:00:59 +0800

    support `and` && `or` symbol condition &&  assoc table search.

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,10 @@
+
+
+import Ecto.Query
+
 alias Turbo.Ecto.Services.BuildSearchQuery
 alias Turbo.Ecto.Hooks.Search
 alias Turbo.Ecto.Hooks.Sort
 alias Turbo.Ecto.Hooks.Paginate
+
+alias Turbo.Ecto.{Category, Product, Variant}
