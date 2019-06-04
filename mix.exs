a969cc19fc0b5046b624b2216d0b09f40a71832d
zven21 <andzven@gmail.com>
2019-06-04 16:28:28 +0800

    upgrade excoveralls version.

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Turbo.Ecto.MixProject do
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0", only: :test},
       {:ex_machina, "~> 2.2", only: :test},
-      {:excoveralls, "~> 0.10", only: :test},
+      {:excoveralls, "~> 0.10.5", only: :test},
       {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
     ]
   end
@@ -62,8 +62,7 @@ defmodule Turbo.Ecto.MixProject do
         "ecto.drop --quiet",
         "ecto.create --quiet",
         "ecto.migrate --quiet",
-        "test",
-        "credo --strict"
+        "test"
       ]
     ]
   end
